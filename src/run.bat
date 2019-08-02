90671c3ebbe0a02b773ded43f4d8011de7ba9a79
LiuYuancheng <liu_yuan_cheng@hotmail.com>
2019-08-02 14:26:33 +0800

    execution program.

---
+++ src/run.bat
@@ -0,0 +1,2 @@
+"C:/Users/dcslyc/AppData/Local/Programs/Python/Python37-32/python.exe" "C:/Singtel/Programs/IOT/IOT/Distribution_Data_Viewer/Distribution_Data_Viewer/src/distributionViewer.py"
+pause
