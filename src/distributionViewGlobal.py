3b36e93990c7e4322c52b896aa6250dfdf1a93b5
solsav <sol.1917@gmail.com>
2019-08-02 15:35:36 +0800

    script running description file added to /misc

--- src/distributionViewGlobal.py
+++ src/distributionViewGlobal.py
@@ -6,7 +6,7 @@
 # Author:      Yuancheng Liu
 #
 # Created:     2019/08/01
-# Copyright:   NUS – Singtel Cyber Security Research & Development Laboratory
+# Copyright:   NUS-Singtel Cyber Security Research & Development Laboratory
 # License:     YC @ NUS
 #-----------------------------------------------------------------------------
 import os
@@ -18,9 +18,9 @@ print("distributionViewGlobal: Current working directory is : %s" %dirpath)
 APP_NAME = 'NetFetcher Distribution Data Viewer'
 
 # module folder:
-MODE_F_PATH = "".join([dirpath, "\\module\\short_rc_exp10.csv"])
+MODE_F_PATH = "".join([dirpath, "/model/short_rc_exp10.csv"])
 # Data folder:
-DATA_F_PATH = "".join([dirpath, "\\data\\short_sc_exp10.csv"])
+DATA_F_PATH = "".join([dirpath, "/data/short_sc_exp10.csv"])
 
 iDataMgr = None          # data manager.
 iChartPanel0 = None      # History chart panel for module
