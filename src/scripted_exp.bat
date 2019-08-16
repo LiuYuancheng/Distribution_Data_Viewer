e914b49fe65ca34445ada7fa917cb3362e07b7ad
LiuYuancheng <liu_yuan_cheng@hotmail.com>
2019-08-16 11:03:58 +0800

    Added the sample data.

--- src/scripted_exp.bat
+++ src/scripted_exp.bat
@@ -1,20 +1,3 @@
-Run: 127.0.0.1:5555 biggerFile:1 1000 exp1
+Run: 192.168.0.0:1232 001:1 10000 rtest
 sleep1
 
-Run: 127.0.0.1:5555 biggerFile:1 1000 exp1
-Sleep 1
-
-Run: 127.0.0.1:5555 biggerFile:1 30000 exp1
-Sleep 1
-
-Run: 127.0.0.1:5555 biggerFile:64 30000 exp6
-Sleep 1
-
-Run: 127.0.0.1:5555 biggerFile:256 30000 exp8
-Sleep 1
-
-Run: 127.0.0.1:5555 biggerFile:512 30000 exp9
-Sleep 1
-
-Run: 127.0.0.1:5555 biggerFile:1024 30000 exp10
-Sleep 1
